bae5c6f98e15be73866392a7106639bad3fa8129
RAKSHIT SHAH <rakshitshah94@users.noreply.github.com>
2015-04-05 20:31:18 +0530

    Update license.php

--- license.php
+++ license.php
@@ -1,6 +1,6 @@
-RAXBOOK License(RB)
+The MIT License (MIT)
 
-Copyright (c) 2014-2017 <Rakshit.in>
+Copyright (c) 2015 RAKSHIT SHAH
 
 Permission is hereby granted, free of charge, to any person obtaining a copy
 of this software and associated documentation files (the "Software"), to deal
@@ -9,74 +9,13 @@ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 copies of the Software, and to permit persons to whom the Software is
 furnished to do so, subject to the following conditions:
 
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
 
 THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
 AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
 LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-
-// <!DOCTYPE html >
-
-// <!-- /**
-//  * @System     HP-pc Rax
-//  * @author     Rakshit shah <Rakshitshah1994@gmail.com>
-//  * @copyright  2015-2017 |Rakshit shah
-//  * @license    http://rakshit.in/license/license.php |  PHP License 3.0
-//  * @version    1.1
-//  * @since      File available since Release 1.0.0
-//  */
-//  -->
-// <html>
-// <head>
-// <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-// <title>License</title>
-// <style>
-// html {background:#fff; height:100%; width:100%;}
-// body {background:#f4f4f4; width:500px; margin:25px auto 25px; text-align:center; display:block; border:solid 1px #ccc; font:normal 14px "Trebuchet MS", Arial, Helvetica, sans-serif; line-height:22px; padding:50px;}
-// a {color:#F60;}
-// article{
-// 	font-size: 20px;
-// 	font-family: times;
-// 	text-align: left;
-// }
-
-
-// </style>
-// </head>
-
-// <body>
-
-// <!-- /**
-//  * @System     HP-pc Rax
-//  * @author     Rakshit shah <Rakshitshah1994@gmail.com>
-//  * @copyright  2015-2017 |Rakshit shah
-//  * @license    http://rakshit.in/license/license.php |  PHP License 3.0
-//  * @version    1.1
-//  * @since      File available since Release 1.0.0
-//  */
-//  -->
-// <article>≡ THIS IS MADE BY RAKSHIT SHAH<br><br>
-// ≡ ABOUT ME<br><br> 
-// Hi Budds !!! I am Rakshit (Rax),a blogger from India.<br>
-// I am founder of RAXBOOK (http://Raxbooks.blogspot.com).
-// <br><br>
-// ≡ STUDY :<br><br>
-// Pursuing Computer Engineering(2012-2016)...<br><br>
-// Having experience with Blogging . (I use Blogger,Wordpress and Drupal.) <br>
-// I have my own themes and templates for wordpress and blogger.
-// <br><br>
-// ≡ WHAT I HAVE MADE : <br><br>
-// Chat application system (php), simple calculator(jquery) , Compass Box (android), tIC-TAC-TOi Game (Android)
-// <br><br>
-// ≡ KNOWN LANGUAGES :<br><br>
-//  Php JS Jsp HTML CSS JQuery Android Java Python Ajax JSON RAX <br><br>
-// ≡ MORE ABOUT ME :<br><br>
-// You can visit my official site : <a href="http://www.Rakshit.in/">Rakshit.in</a>
-// </article>
-// </body>
-// </html>
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
